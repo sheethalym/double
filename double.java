5d74f17c30ed667eb3ef2c852b68f9acee8c066f
sheethalym <sheethal.xworkz@gmail.com>
2021-12-20 21:44:08 +0530

    adding double program

---
+++ double.java
@@ -0,0 +1,17 @@
+class Double{
+ public static void main(String[]args){
+  double rr[]={1.012,1.134,-0.123,1.11,0.22,0.2234526,-1.22,0.162,1.54,0.89};
+  System.out.println(rr[0]);
+ System.out.println(rr[1]);
+ System.out.println(rr[2]);
+ System.out.println(rr[3]);
+ System.out.println(rr[4]);
+ System.out.println(rr[5]);
+ System.out.println(rr[6]);
+ System.out.println(rr[7]);
+ System.out.println(rr[8]);
+ System.out.println(rr[9]);
+
+ }
+
+}
